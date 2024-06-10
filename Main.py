de32a3ac6ca19a4a1a5e499b160a1a858c2192c7
PabloSoligo2014 <pablosoligo1976@gmail.com>
2024-06-10 13:41:36 -0300

    Iteraciones sobre listas

--- Main.py
+++ Main.py
@@ -1,19 +1,23 @@
 #coding=utf8
 #->La linea anterior habilita al uso de caracteres no ascii en los mensajes
+'''
+Created on 9 ago. 2019
 
+@author: pabli
+'''
 
 
 if __name__ == '__main__':
     #->Asignacion con = 
-    midpoint = 5
+    midpoint = 500
     
     #->Instancio dos listas vacias ==> lower = list()
     #->Con ; salto la necesidad de nueva linea para terminar sentencia (No recomendado)
     lower = []; upper = [];
     
-    #->separo los numeros en "altos" y "bajos"
-    #->Iteracion definida, observar que la tabulacion define el bloque!!
-    for i in range(10):
+    
+    #->Iteracion definida, inicio en 10, finalizo en 1000 y los saltos son de 27 (Pueden ser negativos)
+    for i in range(10,1000,27):
         #->Condicional
         if (i<midpoint):
             #->Agrego a la lista
@@ -24,6 +28,14 @@ if __name__ == '__main__':
             
     print("Lower:", lower)
     print("Upper:", upper)
+    
+    #->Iteraciones sobre listas, se crea la lista utilizando nombre clase
+    word_list = list()
+    
+    word_list.extend(["Hola", "Mundo", "!"])
+    
+    for word in word_list:
+        print(word)
     
     
     
