e0f0c4c7c2bb2f6c276bf12ae336e4ba2a9c944f
PabloSoligo2014 <pablosoligo1976@gmail.com>
2024-06-10 13:51:37 -0300

    Busqueda_Orden_Generico

--- Main.py
+++ Main.py
@@ -33,12 +33,54 @@ class Person(object):
             return self.__age>other.__age
         else:
             return False #->O exception?
+        
+
+            
+
+def cmp_person_by_name_age(p1,p2):
+    if p1.get_name()>p2.get_name():
+        return 1
+    elif p1.get_name()<p2.get_name():
+        return -1
+    else:
+        if p1.get_age()>p2.get_age():
+            return 1
+        elif p1.get_age()<p2.get_age():
+            return -1
+        else:
+            return 0
+    
+   
 
 def cmp_person_by_age(p):
     return p.get_age()
 
 def cmp_person_by_name(p):
     return p.get_name()
+
+def find(li, obj, cmp):
+    for el in li:
+        if cmp(el, obj)==0:
+            return el
+    return None
+
+def sort(li, cmp):
+    l = len(li)
+    i = 0
+    while(i<l):
+        min = i
+        for x in range(i+1,l):
+            if cmp(li[x],li[min])<0:
+                min = x
+        
+        if min!=i:
+            aux     = li[i]
+            li[i]    = li[min]
+            li[min]  = aux
+            
+                
+        i = i+1
+    
     
 if __name__ == '__main__':
     persons = [Person("Juan", 30, 1.78), Person("Juan", 35, 1.78), Person("Pedro", 79, 1.50), Person("Maria", 21, 1.68)]
@@ -48,7 +90,20 @@ if __name__ == '__main__':
     persons.sort(key=cmp_person_by_name)
     print(persons)
     
+    j1 = Person("Juan", 35, 1.60)
+    j2 = Person("Juan", 99, 1.60)
     
+    wanted = find(persons, j1, cmp_person_by_name_age)
+    if wanted!=None:
+        print("Buscado y encontrado:", wanted)
+    
+    wanted = find(persons, j2, cmp_person_by_name_age)
+    
+    if wanted==None:
+        print("Buscado y no encontrado")
+        
+    sort(persons, cmp_person_by_name_age)
+    print(persons)
     
     
     
