c6c63931f88d5e2588c8c479ab51ff24ad831bb9
PabloSoligo2014 <pablosoligo1976@gmail.com>
2024-06-10 13:44:48 -0300

    Iteracion indefinida-Incremento

--- Main.py
+++ Main.py
@@ -6,19 +6,40 @@ Created on 9 ago. 2019
 @author: pabli
 '''
 
-def main():
-    #->Entrada por consola, la vamos a usar muy poco.
-    x = input("Ingrese un valor de coma flotante: ")
-    
-    
-    print("El valor ingresado es: ", x, ".")
-    print("El valor ingresado es: %5.2f."%(float(x)))
-    
-    
-
 
 if __name__ == '__main__':
-    main()
+    #->Asignacion con = 
+    midpoint = 500
+    
+    #->Instancio dos listas vacias ==> lower = list()
+    #->Con ; salto la necesidad de nueva linea para terminar sentencia (No recomendado)
+    lower = []; upper = [];
+    
+    
+    #->Iteracion definida, inicio en 10, finalizo en 1000 y los saltos son de 27 (Pueden ser negativos)
+    for i in range(10,1000,27):
+        #->Condicional
+        if (i<midpoint):
+            #->Agrego a la lista
+            lower.append(i)
+        else:
+            #->Agrego a la lista
+            upper.append(i)
+            
+    print("Lower:", lower)
+    print("Upper:", upper)
+    
+    #->Iteraciones sobre listas, se crea la lista utilizando nombre clase
+    word_list = list()
+    word_list.extend(["Hola", "Mundo", "!"])
+
+    #->Ciclo indefinido
+    i=0
+    wll = len(word_list)
+    while(i<wll):
+        #->A los elementos de la lista se los puede acceder por subindices 
+        print(word_list[i])
+        i+=1 #->Operacion valida como en C/C++ => i=i+1, no es posible i++
     
     
         
