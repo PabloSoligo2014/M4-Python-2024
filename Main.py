79e3ff9369429cc46ceef33f4e757a89482a13e4
PabloSoligo2014 <pablosoligo1976@gmail.com>
2024-06-10 13:59:51 -0300

    Archivos Binarios

--- Main.py
+++ Main.py
@@ -5,19 +5,49 @@ Created on 9 ago. 2019
 
 @author: pabli
 '''
+import struct
+"""
+typedef struct {
+    unsigned long dni;       => 4 bytes     => L
+    char nomyap[100];        => 100 bytes   => s
+    float saldo;             => 4 bytes     => f   
+    char sexo;               => 1 byte      => c 
+    char estado;             => 1 bytes     => c
+}t_cliente;
 
-#import random
+Total 110, en realidad es 112 por padding
 
-from random import randrange
-import sys
+"22000001|Jan Ceulemans|10000.25|F|A\n"
+"22000005|Jean-Marie Pfaff|1233.15|M|A\n"
+"25127140|Nilton Santos|1444.99|M|A\n"
+"33351231|Roger Milla|123554.00|M|A\n"
+"""
+import ctypes
 
 if __name__ == '__main__':
-    cant_arg = len(sys.argv)
-    print(type(sys.argv))
-    if sys.argv[1]=="FS2017":
-        filename = "FS2017raw_tlmy_146bytes.bin"
-        pktsize  = sys.argv[2]
-    else:
-        print("Ha ocurrido un error en los parámetros")
-
+    
+    #->Binarios...
+    f = open('./Resources/clientes.dat', mode='rb')
+    
+    chunk = f.read(112)
+    while(chunk):
+        code, name, balance, sex, state, aux= struct.unpack("@L100sfcch", chunk)
+        name = ctypes.create_string_buffer(name).value
+        print(code, str(name, "ASCII"), balance, str(sex, "ASCII"), str(state, "ASCII"))
+        chunk = f.read(112)
+    
+    f.close()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
         
+    
+    
