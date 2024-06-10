800e0872ab460f5da817990101f1fe9e5d4f0558
PabloSoligo2014 <pablosoligo1976@gmail.com>
2024-06-10 13:36:39 -0300

    IO

--- Main.py
+++ Main.py
@@ -1,9 +1,22 @@
 #coding=utf8
 #->La linea anterior habilita al uso de caracteres no ascii en los mensajes
+'''
+Created on 9 ago. 2019
+
+@author: pabli
+'''
 
 
 if __name__ == '__main__':
-    pass
+    #->Entrada por consola, la vamos a usar muy poco.
+    x = input("Ingrese un valor de coma flotante: ")
+    
+    
+    print("El valor ingresado es: ", x, ".")
+    print("El valor ingresado es: %5.2f."%(float(x)))
+    
+    
+    
         
     
     
