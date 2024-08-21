80b28521eb6a3e11c4b015a08d021e84b315c7be
psoligo@unlam.edu.ar <psoligo@unlam.edu.ar>
2024-08-21 16:51:30 -0300

    pyt

--- Main.py
+++ Main.py
@@ -1,9 +1,9 @@
 #coding=utf8
 #->La linea anterior habilita al uso de caracteres no ascii en los mensajes
 
-
+#python -m compileall Main.py
 if __name__ == '__main__':
-    pass
+    print("Hola mundo")
         
     
     
