6e43c0c600ffb660841ba1a13c4a101bc45c575e
psoligo@unlam.edu.ar <psoligo@unlam.edu.ar>
2024-08-22 17:10:39 -0300

    Argumentos

--- Main.py
+++ Main.py
@@ -4,9 +4,35 @@
 #python -m compileall Main.py
 
 
+def arg_como_tuplas(*args):
+    print(type(args))
+    ar1, ar2, ar3 = args
+    print(ar1, ar2, ar3)
+    return args
+
+def arg_como_dic(**args):
+    print(type(args))
+    print(args)
+
+def arg_variables(*args, **kwargs):
+    print("args=>", args)
+    print("kwargs=>", kwargs)
+    return args, kwargs
+
+
+class MiClass(object):
+    _f = 0
+    def __init__(self):
+        print("Constructor")
+        self._f = 1
+
 
 if __name__ == '__main__':
     print("Hola mundo")
+
+    arg_como_tuplas(1, 2, 3)
+    arg_como_dic(a=1, b=2, c=3)
+    arg_variables(1, 2, 3, a=1, b=2, c=3)
         
     
     
