69accb02d2160ee0a203f483fe8ad3c9c4465aa0
psoligo@unlam.edu.ar <psoligo@unlam.edu.ar>
2024-08-23 16:31:31 -0300

    Reflexion de software

---
+++ Calib.py
@@ -0,0 +1,9 @@
+
+calibVar01 = "rawValue*0.5+1"
+
+def calibVar02(x):
+    return x*0.2+1
+
+def calibVar03(x):
+    return x*0.3+2
+
--- Main.py
+++ Main.py
@@ -3,10 +3,10 @@
 
 #python -m compileall Main.py
 
+from Calib import *
 
 def miFun():
     print("Llamando a una funcion")
-
 
 class Alarm(object):
     _f = 0
@@ -43,6 +43,26 @@ if __name__ == '__main__':
     att = getattr(c, '_criticity')
 
     print("Criticidad: %d" % att)
+
+    #eval(expression, globals = None, locals = None)
+    rawValue = 10
+    #print(eval(calb, globals(), locals())) #locals()["rawValue"]
+    print(eval(calibVar01, None, {"rawValue":rawValue})) 
         
     
+    import importlib
+    import inspect
+
+    vars = {"Var02":(10, "calibVar02"), "Var03":(20, "calibVar03")}
+    modulo = importlib.import_module('Calib')
+    fcs = {nombre: funcion for nombre, funcion in inspect.getmembers(modulo, inspect.isfunction)}
     
+
+    for v in vars:
+        value, calfunName = vars[v]
+        f = fcs.get(calfunName)
+        if f:
+            rslt = f(value)
+            print("Resultado de %s: %f" % (v, rslt))
+
+   
