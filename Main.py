b65c64e7dfa3fbd30e32bbb0b0ff4f8cfeefcd26
psoligo@unlam.edu.ar <psoligo@unlam.edu.ar>
2024-08-26 10:52:11 -0300

    Sobrecarga de metodos

--- Main.py
+++ Main.py
@@ -26,14 +26,32 @@ def arg_variables(*args, **kwargs):
 
 class MiClass(object):
     _f = 0
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         print("Constructor")
-        self._f = 1
+
+        self._f = kwargs["f"]
+    
+    def __eq__(self, other):
+        return self._f == other._f
+    
+    def __ne__(self, other):
+        return not self.__eq__(other)
+    
+    def __gt__(self, other):
+        return self._f > other._f
+    
+
 
 
 if __name__ == '__main__':
     print("Hola mundo")
 
+    c1 = MiClass(f=5)
+    c2 = MiClass(f=1)
+    if c1 > c2:
+        print("C1 es mayor que c2")
+    else:
+        print("c1 es menor que c2")
 
 
     """
