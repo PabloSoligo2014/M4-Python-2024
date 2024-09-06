5fcdcee0111ec68d634709e7603eb775c8d9af30
psoligo@unlam.edu.ar <psoligo@unlam.edu.ar>
2024-09-06 09:31:20 -0300

    Reflexion

--- 00_Lect.py
+++ 00_Lect.py
@@ -7,7 +7,43 @@
 #hasattr
 
 
+from GeoClasses import Place, Geolocation
+
 
 
 if __name__ == '__main__':
-    pass
+    p1 = Place(lat=19.4326, lon=-99.1332, description="CDMX")
+    p2 = Place(lat=25.7617, lon=-80.1918, description="Miami")
+    g1 = Geolocation(lat=19.4326, lon=-99.1332)
+    print(p1)
+    print(p2)
+
+    if not isinstance(g1, Place):
+        print("g1 no es instancia de Place") 
+
+    if type(p1) is Place:
+        print("p1 es exactamente una instancia de Place")
+    else:
+        print("p1 no es exactamente una instancia de Place")
+
+
+    if isinstance(p1, Geolocation):
+        print("Es una instancia de Geolocation") 
+    
+    if issubclass(Place, Geolocation):
+        print("Place es subclase de Geolocation")
+
+    if(hasattr(p1, "lat")):
+        print("si lo tiene: ", getattr(p1, "lat"))
+    
+    if(hasattr(p1, "distance")):
+        print("si tiene algo llamado distance")
+        method = getattr(p1, "distance")
+        if callable(method):
+            result = method(p2)  
+            print("Distance: ", result)
+    
+    """
+    print(p1.__class__)
+    print(p2.__class__)
+    """
---
+++ GeoClasses.py
@@ -0,0 +1,104 @@
+#coding=utf8
+#->La linea anterior habilita al uso de caracteres no ascii en los mensajes
+'''
+Created on 9 ago. 2019
+
+@author: pablo
+'''
+
+#import random
+from math import sin, cos, sqrt, atan2, radians
+
+R = 6373.0
+
+class IPersistible(object):
+    def save(self):
+        pass
+    
+
+class Geolocation(object):
+    __lat = 0
+    __lon = 0
+
+    @property
+    def lat(self):
+        return self.__lat
+    @property
+    def lon(self):
+        return self.__lon   
+
+    def __init__(self, *args, **kwargs):
+        super(Geolocation, self).__init__()
+        self.set_lat(kwargs["lat"])
+        self.set_lon(kwargs["lon"])
+        
+    def set_lat(self, value):
+        if value < -90 or value > 90:
+            raise ValueError("Latitud fuera de rango")
+        self.__lat = value
+
+    def set_lon(self, value):
+        if value < -180 or value > 180:
+            raise ValueError("Longitud fuera de rango")
+        self.__lon = value
+
+    def get_lat(self):
+        return self.__lat
+    
+    def get_lon(self):
+        return self.__lon
+    
+    def __str__(self):
+        return "Latitud: " + str(self.__lat) + " Longitud: " + str(self.__lon)
+    
+    def distance(self, other):
+        lat1 = radians(self.__lat)
+        lon1 = radians(self.__lon)
+        lat2 = radians(other.lat)
+        lon2 = radians(other.lon)
+        
+        dlon = lon2 - lon1
+        dlat = lat2 - lat1
+        
+        a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+        c = 2 * atan2(sqrt(a), sqrt(1 - a))
+        
+        return R * c
+    
+    def __sub__(self, other):
+        return self.distance(other)
+    
+    
+class Place(Geolocation):
+
+    __description = ""
+
+    @property
+    def description(self):
+        return self.__description
+
+    def __init__(self, *args, **kwargs):
+        super(Place, self).__init__(*args, **kwargs)
+        if "description" in kwargs:
+            self.__description = kwargs["description"]
+    
+    
+if __name__ == '__main__':
+    
+   
+    g = Geolocation(lon=30.5, lat=5.5)
+    g.__lat = 10
+
+    
+    
+    print(g.lat, g.lon)
+    print(g.__lat, g)
+
+    gConae  = Place(lat=-34.616838, lon=-58.369501, description="CONAE")
+    gUNLaM  = Geolocation(lat=-34.669898, lon=-58.561815)
+    
+    
+    
+    print("La distancia entre la CONAE y la UNLAM es de %5.2f km"%gConae.distance(gUNLaM))
+    print("La distancia entre la CONAE y la UNLAM es de %5.2f km"%(gConae-gUNLaM))
+    
